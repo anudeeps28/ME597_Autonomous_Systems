83f47c807296b21b799bf5b1c883b6de2c83d2c8
Anudeep Sharma <anudeepsharma28@gmail.com>
2022-02-04 13:07:05 -0500

    initial commit

---
+++ build/lab_1_pkg/CMakeFiles/lab_1_pkg_generate_messages_py.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "/home/parallels/catkin_ws/devel/lib/python3/dist-packages/lab_1_pkg/msg/_CustomMessage.py"
+  "/home/parallels/catkin_ws/devel/lib/python3/dist-packages/lab_1_pkg/msg/__init__.py"
+  "/home/parallels/catkin_ws/devel/lib/python3/dist-packages/lab_1_pkg/srv/_CustomService.py"
+  "/home/parallels/catkin_ws/devel/lib/python3/dist-packages/lab_1_pkg/srv/__init__.py"
+  "CMakeFiles/lab_1_pkg_generate_messages_py"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/lab_1_pkg_generate_messages_py.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
